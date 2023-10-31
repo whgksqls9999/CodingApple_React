1721784e8342df879fd46e29fdbbf8d6530f12fd
Jo hanbin <whgksqls9999@naver.com>
2023-11-01 00:48:30 +0900

    02-JSX Syntax

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+
   return (
    <div className="App">
-
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <h4>{ post }</h4>
    </div>
   );
 }
