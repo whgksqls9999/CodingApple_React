256c73f9d52c75f7fc969ab8a8fb53c9e20733e8
Jo hanbin <whgksqls9999@naver.com>
2023-11-01 01:03:04 +0900

    03-state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [title, b] = useState(['강남 우동 맛집', '게시글 제목2', '게시글 제목 3']);
 
   return (
    <div className="App">
       <div className="black-nav">
         <h4>블로그임</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ title[0] }</h4>
+        <p> 11월 1일 발행 </p>
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p> 11월 1일 발행 </p>
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p> 11월 1일 발행 </p>
+      </div>
    </div>
   );
 }
